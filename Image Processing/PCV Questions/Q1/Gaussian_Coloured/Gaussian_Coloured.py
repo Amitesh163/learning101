65fae154f2e1f6b024155304f96f885a7da0772a
Amitesh Singh Sisodia <60354563+Amitesh163@users.noreply.github.com>
2020-03-28 23:23:35 +0530

    Add files via upload

---
+++ Image Processing/PCV Questions/Q1/Gaussian_Coloured/Gaussian_Coloured.py
@@ -0,0 +1,21 @@
+#required modules
+from PIL import Image
+from numpy import *
+from scipy.ndimage import filters
+
+#accessing the image
+im = array(Image.open('image.jpg'))
+im2 = zeros(im.shape)
+
+#applying Gaussian blurring to each colour
+for i in range(3):
+    im2[:,:,i] = filters.gaussian_filter(im[:,:,i],20)
+
+#conversion to uint8 form
+im2 = uint8(im2)
+
+#obtaining image from the image array
+im2 = Image.fromarray(uint8(im2))
+
+#saving the image
+im2.save('Gaussian_coloured(sigma=20).jpg')
